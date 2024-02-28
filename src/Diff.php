ec1ff41abe6338adcde0b8a831ee4bf14ec7544e
NYakovlev <n.yakovlev96@gmail.com>
2024-02-28 19:19:47 +0500

    feat: add tests

--- src/Diff.php
+++ src/Diff.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\GetDiff;
+namespace App\Diff;
 
 function gendiff($pathToFile1, $pathToFile2)
 {
---
+++ tests/DiffTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function App\Diff\gendiff;
+
+class DiffTest extends TestCase
+{
+    public function testDiff(): void
+    {
+        $filepath1 = 'tests/fixtures/filepath1.json';
+        $filepath2 = 'tests/fixtures/filepath2.json';
+        $expected = file_get_contents(__DIR__ . '/fixtures/expected.txt');
+        $this->assertEquals($expected, gendiff($filepath1, $filepath2));
+    }
+}
